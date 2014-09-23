7cb8c83c4a714dbc556bd8270378b5212bddada4
Kyle Richardson <richarkc@miamioh.edu>
2014-09-23 17:38:52 -0400

    Added documentation, cleaned up code slightly.

--- Lab4Server.java
+++ Lab4Server.java
@@ -5,6 +5,13 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
+@SuppressWarnings("resource")
+
+/**
+ * Multithreaded server for generating and sending AMP(A Math Problem) to clients.
+ * @author Kyle Richardson
+ *
+ */
 public class Lab4Server implements Runnable {
 	private static int complete, threads, correct, incorrect;
 	private static final int PORT = 4242;
@@ -13,6 +20,14 @@ public class Lab4Server implements Runnable {
 	private BufferedReader in;
 	private double serverAnswer;
 
+	/**
+	 * Initiates new instance of the AMP server.
+	 * 
+	 * @param sock
+	 *            Socket of the client
+	 * @throws IOException
+	 *             General IO exception.
+	 */
 	public Lab4Server(Socket sock) throws IOException {
 		clientSock = sock;
 		out = new PrintWriter(clientSock.getOutputStream(), true);
@@ -21,34 +36,67 @@ public class Lab4Server implements Runnable {
 	}
 
 	public static void main(String args[]) throws IOException {
-		
+
 		ServerSocket sock = new ServerSocket(PORT);
 		while (true) {
-			Socket client = sock.accept();
-			(new Thread(new Lab4Server(client))).start();
+			try {
+				Socket client = sock.accept();
+				(new Thread(new Lab4Server(client))).start();
+			} catch (SocketException e) {
+				e.printStackTrace();
+				System.out.println("Could not open socket. Terminating");
+				System.exit(1);
+			}
+
 		}
 
 	}
+
+	/**
+	 * Increments tracking of threads.
+	 */
 
 	private static synchronized void incThread() {
 		threads++;
 	}
 
+	/**
+	 * Decrements tracking of threads.
+	 */
 	private static synchronized void decThread() {
 		threads--;
 	}
+
+	/**
+	 * Increments the number of correct AMP's.
+	 */
 
 	private static synchronized void incCorrect() {
 		correct++;
 	}
 
+	/**
+	 * Increments the number of incorrect AMP's.
+	 */
 	private static synchronized void incIncorrect() {
 		incorrect++;
 	}
-	
+
+	/**
+	 * Increments the number of completed AMP's.
+	 */
+
 	private static synchronized void incComplete() {
 		complete++;
 	}
+
+	/**
+	 * Sends a math problem to the client to solve, and calculates answer for
+	 * correctness.
+	 * 
+	 * @throws ScriptException
+	 *             Script does not properly eval the problem
+	 */
 
 	private void sendProblem() throws ScriptException {
 		out.println("AMP WORK");
@@ -62,6 +110,12 @@ public class Lab4Server implements Runnable {
 				+ op2);
 		out.println(op1 + " " + operation + " " + op2);
 	}
+
+	/**
+	 * Calculates randomly a math operation for calculations.
+	 * 
+	 * @return Math operation in string form.
+	 */
 
 	private String getOperation() {
 		switch ((int) Math.round(Math.random() * 3)) {
@@ -78,12 +132,25 @@ public class Lab4Server implements Runnable {
 
 	}
 
+	/**
+	 * Returns server status to client.
+	 */
 	private void getStatus() {
 		out.println("AMP STATUS");
 		String status = "THREADS = " + threads + ", CORRECT = " + correct
 				+ ", INCORRECT = " + incorrect;
 		out.println(status);
 	}
+
+	/**
+	 * Reads in client input, determines if answer is correct, then sends back
+	 * to client that it has received the answer.
+	 * 
+	 * @throws NumberFormatException
+	 *             Client's answer in incorrect format.
+	 * @throws IOException
+	 *             General IOException.
+	 */
 
 	private void putAnswer() throws NumberFormatException, IOException {
 		double answer = Double.parseDouble(in.readLine());
@@ -93,7 +160,7 @@ public class Lab4Server implements Runnable {
 		else
 			incIncorrect();
 		out.println("AMP OK");
-		
+
 	}
 
 	@Override
@@ -109,12 +176,11 @@ public class Lab4Server implements Runnable {
 						if (complete < 10000) {
 							incComplete();
 							sendProblem();
-							
-							
+
 						} else {
 							System.out.println("No work Available for client.");
 							out.println("AMP NONE");
-							
+
 						}
 						break;
 					}
@@ -127,7 +193,8 @@ public class Lab4Server implements Runnable {
 						break;
 					}
 					case ("END SESSION"): {
-						System.out.println("Client requesting termination, terminating connection");
+						System.out
+								.println("Client requesting termination, terminating connection");
 						break loop;
 					}
 
@@ -142,14 +209,12 @@ public class Lab4Server implements Runnable {
 				}
 
 			} catch (NumberFormatException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				System.out
+						.println("Error: Client's answer not in proper format.");
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				System.out.println("Error: IO error.");
 			} catch (ScriptException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				System.out.println("Error: AMP did not evaluate properly");
 			}
 		}
 		decThread();
@@ -157,9 +222,8 @@ public class Lab4Server implements Runnable {
 		try {
 			clientSock.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("Error: Client socket did not close properly");
 		}
-		
+
 	}
 }
