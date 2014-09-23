37584a992d7634e926e93d34401eb2ebf56819fe
Kyle Richardson <richarkc@miamioh.edu>
2014-09-23 17:09:00 -0400

    Cleaned up source, added multithreading

--- Lab4Server.java
+++ Lab4Server.java
@@ -1,13 +1,12 @@
 import java.net.*;
 import java.io.*;
 
-
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
 public class Lab4Server implements Runnable {
-	private int complete, threads, correct, incorrect;
+	private static int complete, threads, correct, incorrect;
 	private static final int PORT = 4242;
 	private Socket clientSock;
 	private PrintWriter out;
@@ -16,35 +15,85 @@ public class Lab4Server implements Runnable {
 
 	public Lab4Server(Socket sock) throws IOException {
 		clientSock = sock;
-		out = new PrintWriter(sock.getOutputStream());
+		out = new PrintWriter(clientSock.getOutputStream(), true);
 		in = new BufferedReader(new InputStreamReader(
 				clientSock.getInputStream()));
 	}
 
 	public static void main(String args[]) throws IOException {
-		@SuppressWarnings("resource")
+		
 		ServerSocket sock = new ServerSocket(PORT);
 		while (true) {
 			Socket client = sock.accept();
 			(new Thread(new Lab4Server(client))).start();
 		}
-		
+
 	}
 
-	private synchronized void incThread() {
+	private static synchronized void incThread() {
 		threads++;
 	}
 
-	private synchronized void decThread() {
+	private static synchronized void decThread() {
 		threads--;
 	}
 
-	private synchronized void incCorrect() {
+	private static synchronized void incCorrect() {
 		correct++;
 	}
 
-	private synchronized void incIncorrect() {
+	private static synchronized void incIncorrect() {
 		incorrect++;
+	}
+	
+	private static synchronized void incComplete() {
+		complete++;
+	}
+
+	private void sendProblem() throws ScriptException {
+		out.println("AMP WORK");
+		String operation = getOperation();
+		double op1 = Math.random() * 100;
+		double op2 = Math.random() * 100;
+		ScriptEngineManager mgr = new ScriptEngineManager();
+		ScriptEngine engine = mgr.getEngineByName("JavaScript");
+		serverAnswer = (double) engine.eval(op1 + operation + op2);
+		System.out.println("Sending client AMP: " + op1 + " " + operation + " "
+				+ op2);
+		out.println(op1 + " " + operation + " " + op2);
+	}
+
+	private String getOperation() {
+		switch ((int) Math.round(Math.random() * 3)) {
+		case 0:
+			return "+";
+		case 1:
+			return "-";
+		case 2:
+			return "*";
+		default:
+			return "/";
+
+		}
+
+	}
+
+	private void getStatus() {
+		out.println("AMP STATUS");
+		String status = "THREADS = " + threads + ", CORRECT = " + correct
+				+ ", INCORRECT = " + incorrect;
+		out.println(status);
+	}
+
+	private void putAnswer() throws NumberFormatException, IOException {
+		double answer = Double.parseDouble(in.readLine());
+		System.out.println("Answer:" + answer);
+		if (answer == serverAnswer)
+			incCorrect();
+		else
+			incIncorrect();
+		out.println("AMP OK");
+		
 	}
 
 	@Override
@@ -57,59 +106,28 @@ public class Lab4Server implements Runnable {
 					switch (input) {
 					case ("GET WORK"): {
 						System.out.println("Client requesting AMP");
-						if (complete < 5) {
-							out.println("AMP WORK");
-							String operation = "";
-							switch ((int) Math.round(Math.random() * 3)) {
-							case 0:
-								operation = "+";
-								break;
-							case 1:
-								operation = "-";
-								break;
-							case 2:
-								operation = "*";
-								break;
-							default:
-								operation = "/";
-								break;
-							}
-							double op1 = Math.random() * 100;
-							double op2 = Math.random() * 100;
-							ScriptEngineManager mgr = new ScriptEngineManager();
-							ScriptEngine engine = mgr
-									.getEngineByName("JavaScript");
-							serverAnswer = (double) engine.eval(op1 + operation
-									+ op2);
-							System.out.println("Sending client AMP: " + op1
-									+ " " + operation + " " + op2);
-							out.println(op1 + " " + operation + " " + op2);
+						if (complete < 10000) {
+							incComplete();
+							sendProblem();
+							
+							
 						} else {
 							System.out.println("No work Available for client.");
 							out.println("AMP NONE");
+							
 						}
 						break;
 					}
 					case ("PUT ANSWER"): {
-						double answer = Double.parseDouble(in.readLine());
-						System.out.println("Answer:" + answer);
-						if (answer == serverAnswer)
-							incCorrect();
-						else
-							incIncorrect();
-						out.println("AMP OK");
-						complete++;
-
+						putAnswer();
 						break;
 					}
 					case ("GET STATUS"): {
-						out.println("AMP STATUS");
-						String status = "THREADS = " + threads + ", CORRECT = "
-								+ correct + ", INCORRECT = " + incorrect;
-						out.println(status);
+						getStatus();
 						break;
 					}
 					case ("END SESSION"): {
+						System.out.println("Client requesting termination, terminating connection");
 						break loop;
 					}
 
@@ -135,6 +153,13 @@ public class Lab4Server implements Runnable {
 			}
 		}
 		decThread();
+
+		try {
+			clientSock.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 }
